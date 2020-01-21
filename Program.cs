d6b21af521bc0a38add329e7104dc81b6fb42a45
Beatrice <beatriceopara94@gmail.com>
2020-01-21 20:41:44 +0000

    Added a simple multiplier

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==========================");
+            Console.WriteLine("Welcome to The Calculator");
+            Console.WriteLine("==========================");
+
+            Console.Write("Enter a number: ");
+            string myFirstNum = Console.ReadLine();
+            int myInteger = int.Parse(myFirstNum);
+            //int.Parse is a method that converts strings to integers
+
+            Console.WriteLine("Please enter another number: ");
+            string mySecondNum = Console.ReadLine();
+            int mySecondInteger = int.Parse(mySecondNum);
+
+            Console.WriteLine(myInteger * mySecondInteger);
+            Console.ReadLine();
+        }
+    }
+}
