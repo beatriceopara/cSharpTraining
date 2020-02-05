528484ffe926941e661e508251384ec38033e91d
Beatrice <beatriceopara94@gmail.com>
2020-02-05 22:56:04 +0000

    refactor added methods

--- Program.cs
+++ Program.cs
@@ -4,69 +4,87 @@ namespace Calculator
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            PrintWelcomeMessage();
+            
+            while(true){
+                PerformOneCalculation();
+            }
+        }
+    
+
+        private static void PrintWelcomeMessage(){
             Console.WriteLine("==========================");
             Console.WriteLine("Welcome to The Calculator");
             Console.WriteLine("==========================");
-            
+        }
+
+        private static void PerformOneCalculation(){
+            var op = AskForOperator();
+            var numbers = AskForAmountOfNumArray(op);
+            var answer = CalculateAnswer(op, numbers);
+
+            Console.WriteLine("The answer is: " + answer);
+            Console.ReadLine();
+        }
+
+        private static string AskForOperator(){
             Console.Write("Please choose a operator: +, -, *, /: ");
             string op = Console.ReadLine();
+            return op;
 
+        }
+
+        private static int[] AskForAmountOfNumArray(string op){
             //declare line to caluclate more than two numbers
-            Console.Write("How many numbers do you want to " + op + " ? ");
-            int counter = int.Parse(Console.ReadLine());
-
+           var counter = AskForNumber("How many numbers do you want to " + op + " ? ");
             //created empty array to store numbers
             int [] nums =  new int[counter];
             //for loop to read nums and put into an array
             for (int i = 0; i < counter; i++)
             {
-                Console.Write("Please enter number " + (i  + 1)  +": ");
-                nums[i] = int.Parse(Console.ReadLine());
+                nums[i] = AskForNumber("Please enter number " + (i  + 1)  +": ");
             }
-            
-            // Console.Write("Enter a number: ");
-            // string myFirstNum = Console.ReadLine();
-            // int myInteger = int.Parse(myFirstNum);
-            // //int.Parse is a method that converts strings to integers
+            return nums;
+        }
+        
+        private static int AskForNumber(string message){
+            int count;
 
-            // Console.WriteLine("Please enter another number: ");
-            // string mySecondNum = Console.ReadLine();
-            // int mySecondInteger = int.Parse(mySecondNum);
+            do {
+                Console.Write(message);
+            } while(!int.TryParse(Console.ReadLine(), out count));
+            return count;
+        }
 
-            // Console.WriteLine(myInteger * mySecondInteger);
-
-            //for loop to go through nums and calculate them altogether
-
+        private static int CalculateAnswer(string op, int[] nums){
             int answer = nums[0];
 
-            for (int i = 1; i < counter; i++)
+            for (int i = 1; i < nums.Length; i++)
             {
                 if(op == "+") {
-                    answer = answer + answer;
+                    answer = answer + nums[i];
                 }
                 else if (op == "-") {
-                    answer = answer - answer;
+                    answer = answer - nums[i];
                 }
 
                 else if (op == "*") {
-                    answer = answer * answer;
+                    answer = answer * nums[i];
                 }
 
                 else if (op == "/") {
-                    answer = answer / answer;
+                    answer = answer / nums[i];
                 }
-
+            
                 else {
                     Console.Write("Invalid operator. Please try again");
                 } 
-
             }
-
-            Console.WriteLine("The answer is: " + answer);
-            Console.ReadLine();
+              return answer;
         }
     }
-
 }
+
+        
